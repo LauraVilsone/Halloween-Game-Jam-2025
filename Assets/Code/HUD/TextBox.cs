abb58d7d7cac2e8fb892c96b490fac0b8d1392a3
Antonio Ackalin <antonioh.ackalin@hotmail.com>
2025-10-31 04:34:27 +0100

    Textbox speaker name label also now shows up and hides itself accordingly

--- Assets/Code/HUD/TextBox.cs
+++ Assets/Code/HUD/TextBox.cs
@@ -8,6 +8,8 @@ public class TextBox : UIGroup
 {
     [SerializeField] private TextMeshProUGUI m_text;
     [SerializeField] private TextMeshProUGUI m_name;
+    [Space]
+    [SerializeField] private Image m_header;
 
     public string Text => m_text.text;
 
@@ -19,9 +21,15 @@ public class TextBox : UIGroup
     public void Name(Actors actor)
     {
         if (actor == Actors.Narrator)
+        {
             m_name.text = "";
+            m_header.gameObject.SetActive(false);
+        }
         else
+        {
             m_name.text = actor.ToString();
+            m_header.gameObject.SetActive(true);
+        }
     }
 
     public void Fill(string text)
