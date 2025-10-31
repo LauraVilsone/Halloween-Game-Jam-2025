22f40907e99e11d6059041956227f6b2606721b6
Antonio Ackalin <antonioh.ackalin@hotmail.com>
2025-10-31 02:59:12 +0100

    bug fix where keyword flashing wouldn't work for keywords made out of multiple words.

--- Assets/Code/HUD/ClickableText.cs
+++ Assets/Code/HUD/ClickableText.cs
@@ -40,18 +40,58 @@ public class ClickableText : MonoBehaviour, IPointerClickHandler, IPointerMoveHa
             var linkID = linkInfo.GetLinkID();
             var linkText = linkInfo.GetLinkText();
 
+            string concatenatedWord = "";
+            int firstCharacterIndex = 0;
+            int lastCharacterIndex = 0;
+
             var success = m_keywordManager.OnKeywordGain(linkID);
             //success = true;
+
             if (success)
             {
                 Vector3 position = Vector3.zero;
-                foreach (var wordInfo in m_text.textInfo.wordInfo)
+                string[] linkWords = linkText.Split(' ');
+                
+                //foreach (var wordInfo in m_text.textInfo.wordInfo)
+                for (int i = 0; i < m_text.textInfo.wordInfo.Length; i++)
                 {
-                    if (wordInfo.characterCount == 0 || wordInfo.GetWord() != linkText)
-                        continue;
+                    var wordInfo = m_text.textInfo.wordInfo[i];
+                    if (wordInfo.textComponent == null) break;
 
-                    var firstCharInfo = m_text.textInfo.characterInfo[wordInfo.firstCharacterIndex];
-                    var lastCharInfo = m_text.textInfo.characterInfo[wordInfo.lastCharacterIndex];
+                    var word = wordInfo.GetWord();
+                    if (wordInfo.characterCount == 0)
+                        continue;
+                    else
+                    {
+                        if (linkText.Contains(word))
+                        {
+                            concatenatedWord = word;
+
+                            var otherWordInfo = wordInfo;
+                            if (linkWords.Length > 1)
+                            {
+                                int j = i + 1;
+                                otherWordInfo = m_text.textInfo.wordInfo[j];
+                                string otherword = "";
+                                for (; j < i + (linkWords.Length); j++)
+                                {
+                                    otherWordInfo = m_text.textInfo.wordInfo[j];
+                                    otherword = otherWordInfo.GetWord();
+                                    concatenatedWord = concatenatedWord + " " + otherword;
+                                }
+                            }
+
+                            if (!linkText.Contains(concatenatedWord))
+                                continue;
+
+                            firstCharacterIndex = wordInfo.firstCharacterIndex;
+                            lastCharacterIndex = otherWordInfo.lastCharacterIndex;
+                        }
+                        else continue;
+                    }
+
+                    var firstCharInfo = m_text.textInfo.characterInfo[firstCharacterIndex];
+                    var lastCharInfo = m_text.textInfo.characterInfo[lastCharacterIndex];
                     var wordLocation = m_text.transform.TransformPoint((firstCharInfo.topLeft + lastCharInfo.bottomRight) / 2f);
 
                     position = wordLocation;
